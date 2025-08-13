7ae8ad780e1272ac65f2d8353972df352e5329a3
hylaea-miller <hylaeamiller@gmail.com>
2025-08-13 16:33:05 -0300

    for loop practice

--- day3InteractiveSession.R
+++ day3InteractiveSession.R
@@ -60,3 +60,70 @@ switch(species,
        "dog" = print("woof"),
        "mouse" = print("squeak"))
 
+# see next week dplyr::case_when()
+# Writing for loops
+
+dog_names <- c("Teddy", "khora", "Banjo", "Waffle")
+
+#create ex. for the one data element to put into for loop
+
+print("my dog's name is Teddy")
+paste("my dog's name is", dog_names[1])
+
+print(paste("my dog's name is", dog_names[1]))
+print(paste("my dog's name is", dog_names[2]))
+print(paste("my dog's name is", dog_names[3]))
+
+# version with a variable that I can update
+
+pupster <- 1
+
+print(paste("my dog's name is", dog_names[pupster]))
+
+# convert to a for loop
+
+for (pupster in dog_names) {
+  print(paste("my dog's name is", pupster))
+}
+
+# Another for loop example
+
+# data that I'm working with
+mass <- seq(from = 0, to = 3, by = 0.5)
+
+i <- mass[1]
+new_val <- i + 2
+print(new_val)
+
+# write into a for loop
+for (i in mass) {
+  new_val <- mass[i] + 2 
+  print(new_val)}
+
+#same thing
+for (i in seq_along(mass)) {
+  new_val <- mass[i] + 2
+  print(new_val)}
+
+# another example with iterating by position ( indexing)
+
+tree_height <- c(1, 2, 6, 10)
+
+# example for the fist case
+tree_height[1] + tree_height[2]
+
+# convert into a generalizable expression
+
+i <- 2
+val <- tree_height[i] + tree_height[i + 1]
+print(val)
+
+
+#convert into a for loop
+
+seq_along(tree_height)
+
+for (i in seq_along(tree_height)) {
+  val <- tree_height[i] + tree_height[i + 1]
+  print(val)
+}
